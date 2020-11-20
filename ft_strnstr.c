76996216261095c1de4f5a8a33ea53f44701b575
Brent Wayfarers <bwayfare@un-p2.kzn.21-school.ru>
2020-11-20 20:25:35 +0300

    Working ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bwayfare <bwayfare@student.21-schoo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 00:52:59 by bwayfare          #+#    #+#             */
-/*   Updated: 2020/11/17 00:53:00 by bwayfare         ###   ########.fr       */
+/*   Updated: 2020/11/20 20:15:47 by bwayfare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@ char	*ft_strnstr(const char *str, const char *find, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (!find)
+	if (!*find)
 		return ((char *)str);
 	while (str[i] && i < len)
 	{
-		if (str[i] == find[j])
+		if (str[i] == find[0])
+		{
 			j = 0;
 			while (find[j] && i + j < len && find[j] == str[i + j])
 			{
-				if (find[j + 1] == '\0')
+				if (!find[j + 1])
 					return ((char *)&str[i]);
 				j++;
 			}
+		}
 		i++;
 	}
 	return (NULL);
