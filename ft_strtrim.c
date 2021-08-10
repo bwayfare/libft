cfe8dc15d913d557295382dfcce61868f76522b9
Brent Wayfarers <bwayfare@student.21-school.ru>
2021-08-10 20:17:45 +0300

    Add README.md

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bwayfare <bwayfare@student.21-schoo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 00:53:02 by bwayfare          #+#    #+#             */
-/*   Updated: 2020/11/17 00:53:03 by bwayfare         ###   ########.fr       */
+/*   Updated: 2020/11/19 20:42:42 by bwayfare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (NULL);
 	begin = 0;
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
 	while (ft_strchr(set, s1[begin]) && s1[begin])
 		begin++;
 	if (s1[begin] == '\0')
 		return (ft_strdup(""));
-	while (ft_strchr(set, s1[end]))
+	while (ft_strchr(set, s1[end - 1]))
 		end--;
-	res = ft_substr(s1, begin, end - begin + 1);
+	res = ft_substr(s1, begin, end - begin);
 	if (!res)
 		return (NULL);
 	return (res);
